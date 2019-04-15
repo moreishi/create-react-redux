b086abfbbb5688cde6a8f5e62cbdda4a9be55e67
Caesar Ian Belza <moreishi@gmail.com>
2019-04-15 16:47:04 +0800

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './components/Home';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+        <Router>
+            <Route path="/" exact component={Home} />
+        </Router>
     );
   }
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../styles/components/_home.scss';
+import { Form, FormGroup } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="col-md-6 offset-md-3 mt-8" style={{marginTop:'250px'}}>
+                <h1 style={{textAlign:'center'}}>The Portfolio</h1>
+                <Form className="col-md-6 offset-md-3">
+                    <FormGroup>
+                        <input placeholder="Enter invite code to continue" style={{textAlign:'center'}} className='form-control form-control-sm' type="text"/>
+                        <button type='button' className='btn btn-sm btn-primary mt-2 offset-5'>Enter</button>
+                    </FormGroup>
+                </Form>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
