60db46a42b25bf5f979a2e3c0c5538054572a75f
Caesar Ian Belza <moreishi@gmail.com>
2019-04-15 18:15:35 +0800

    update applications page

--- src/components/Applications/index.js
+++ src/components/Applications/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Application = () => {
     return (
-        <div className="container">
+        <div className="container mb-8">
             <div className="row">
                 <div className="col-md-8 offset-2 mt-5">
                     <h1>Applications</h1>
-
                     <div className="row">
                         <div className="col-md-12 mt-5 bb-primary">
                             <div className="media">
@@ -14,7 +13,6 @@ const Application = () => {
                                     <div className="media-body">
                                         <h5 className="mt-0">Remittance</h5>
                                         <p><small>What this application does is that it allows you to send money to anyone to any location. It calculates from the base currency to the currency of your choice.</small></p>
-                                        <p><small>Note: This is a side project and not for production mode.</small></p>
                                         <ul className='social-icons'>
                                             <li><h6><span className="badge badge-secondary badge-primary-light">javascript</span></h6></li>
                                             <li><h6><span className="badge badge-secondary badge-primary-light">react</span></h6></li>
@@ -26,6 +24,47 @@ const Application = () => {
                                             <li><a href=""><i className="fab fa-github fa-2x primary-light"></i></a></li>
                                         </ul>
                                     </div>
+                            </div>
+                        </div>
+                        <div className="col-md-12 mt-5 bb-primary">
+                            <div className="media">
+                                <img width="110px" src="https://scontent.fmnl6-2.fna.fbcdn.net/v/t1.0-1/p320x320/19989418_280397779103104_30105891001568524_n.jpg?_nc_cat=106&_nc_eui2=AeEyQjTVaNljVRbUNcvQ7R68GRlrWoV8wf1bVLRgsVHSx9D5FaGTePMc6LC04XTMHGjjNkzUSUX5zwF7paFd1-5m96HzOrE48IAVcKlcs-DMwA&_nc_pt=1&_nc_ht=scontent.fmnl6-2.fna&oh=d73c43a1bb381b6d925bedd824ffe644&oe=5D4AEAC4" className="mr-3" alt="" />
+                                <div className="media-body">
+                                    <h5 className="mt-0">Splash Image</h5>
+                                    <p><small>This application allows you to search a free high-resolution photos. You can download any images in high resolution. All images downloaded are free of charge and you can use any any means you like.</small></p>
+                                    <ul className='social-icons'>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">javascript</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">react</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">css3</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">html5</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">restful api</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">web service</span></h6></li>
+                                        <li><a href="#" style={{fontSize:'12px',color:'#fff'}}>view demo</a></li>
+                                    </ul>
+                                    <ul className='social-icons'>
+                                        <li><a href=""><i className="fab fa-github fa-2x primary-light"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-12 mt-5 bb-primary">
+                            <div className="media">
+                                <img width="110px" src="https://scontent.fmnl6-2.fna.fbcdn.net/v/t1.0-1/p320x320/19989418_280397779103104_30105891001568524_n.jpg?_nc_cat=106&_nc_eui2=AeEyQjTVaNljVRbUNcvQ7R68GRlrWoV8wf1bVLRgsVHSx9D5FaGTePMc6LC04XTMHGjjNkzUSUX5zwF7paFd1-5m96HzOrE48IAVcKlcs-DMwA&_nc_pt=1&_nc_ht=scontent.fmnl6-2.fna&oh=d73c43a1bb381b6d925bedd824ffe644&oe=5D4AEAC4" className="mr-3" alt="" />
+                                <div className="media-body">
+                                    <h5 className="mt-0">Quote2Text</h5>
+                                    <p><small>This application allows you to send quote sms to your friends mobile phones. This is to show that the portfolio uses third-party API web service. To avoid misuse of the application, you can enjoy upto 3 sms credits.</small></p>
+                                    <ul className='social-icons'>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">javascript</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">react</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">css3</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">html5</span></h6></li>
+                                        <li><h6><span className="badge badge-secondary badge-primary-light">nodejs</span></h6></li>
+                                        <li><a href="#" style={{fontSize:'12px',color:'#fff'}}>view demo</a></li>
+                                    </ul>
+                                    <ul className='social-icons'>
+                                        <li><a href=""><i className="fab fa-github fa-2x primary-light"></i></a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
